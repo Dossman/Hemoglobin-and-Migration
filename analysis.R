04da54e992334c2943d2b47363f1a5d35829676c
Dossman <bdossman@gmail.com>
2017-04-20 22:01:48 -0500

    added some code to run HB analysis

--- analysis.R
+++ analysis.R
@@ -1,1 +1,24 @@
-## Analysis
+####################################################################################################
+##
+## Birds Elevate Hemoglobin prior to Spring Migration
+##
+####################################################################################################
+## Libraries
+library(tidyverse)
+
+## Read in data
+blood <- read.csv("./data/blood.csv", sep="\t")
+
+## format dataset
+
+blood$date <- as.POSIXct(blood$date, "%d-%b-%y", tz="EST")
+blood$doy  <- as.numeric(format(blood$date, "%j"))
+blood$woy  <- as.numeric(format(blood$date, "%W"))
+blood$moy  <- as.numeric(format(blood$date, "%m"))
+
+## Plotting raw timeseries data
+
+ggplot(data =blood, aes(x = factor(moy), y=rbc/whole, color=species)) +
+  geom_boxplot()
+
+
